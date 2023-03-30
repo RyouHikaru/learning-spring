89524fbfb4ee042a24efd16fdb9a9a5f2f617274
RyouHikaru <carljamesreblora13@gmail.com>
2023-03-30 14:44:16 +0800

    Added Dependency checks, Inner beans, and Scope

---
+++ src/main/java/com/tapioca/spring/springcore/dependencycheck/Test.java
@@ -0,0 +1,16 @@
+package com.tapioca.spring.springcore.dependencycheck;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/tapioca/spring/springcore/dependencycheck/config.xml");
+
+		Prescription prescription = (Prescription) context.getBean("prescription");
+		System.out.println(prescription);
+	}
+
+}
